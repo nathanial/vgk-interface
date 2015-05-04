db2a1d8bd017cf2f788f4c5fcf6405e574687294
Nathanial Hartman <nathanial.hartman@gmail.com>
2015-05-03 21:47:39 -0700

    added engine

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 var React = require('react');
+var VoxelTypes = require('./data/VoxelTypes');
+var _ = require('lodash');
 var HUD = require('./hud/HUD');
 var $ = require('jquery');
+var engine = require('./engine/coherent');
+
+engine.on('Ready', function(){
+  engine.call('GetVoxelRegistry').then(function(VoxelRegistry){
+    _.each(VoxelTypes, function(VoxelType){
+      VoxelRegistry.RegisterFromJSON(JSON.stringify(VoxelType));
+    });
+  });
+});
+
 $(function(){
   React.render(
     React.createElement(HUD, null),
     document.body
-  );  
+  );
 });
